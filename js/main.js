6e815bf8eb6ffce392415401ef42764308459e5e
Paul Baker <p.baker1@yahoo.com>
2018-08-04 12:38:29 -0500

    background

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Loads hamburger menu for mobile
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+// Controls floating action button
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {direction: 'bottom'});
+});
