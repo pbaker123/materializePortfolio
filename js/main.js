dde349f0918bc97286ec90bdfb567a3017d5c539
Paul Baker <p.baker1@yahoo.com>
2018-08-04 22:29:18 -0500

    finished new portfolio

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
-// Loads hamburger menu for mobile
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
+  var options = {
+    	inDuration: 300,
+      outDuration: 225,
+      coverTrigger: false, // Displays dropdown below the button
+    };
+  var instances = M.Sidenav.init(elems, options);
 });
 
-// Controls floating action button
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {direction: 'bottom'});
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var options = {
+  	inDuration: 300,
+    outDuration: 225,
+    coverTrigger: false, // Displays dropdown below the button
+    alignment: 'right' // Displays dropdown with edge aligned to the left of button
+  };
+  var instances = M.Dropdown.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var options = {
+  };
+  var instances = M.Carousel.init(elems, options);
 });
